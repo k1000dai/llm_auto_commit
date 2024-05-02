ce388b20b7018bf82fb201f4ff330d21d09ef091
k1000dai <chiyodakku1000@gmail.com>
2024-05-02 14:37:13 +0900

    Add setup.py file with required setuptools configuration for gitautocommit package

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+import setuptools
+
+setup(
+    name="gitautocommit",
+    version="0.1.0",
+    packages=setuptools.find_packages(),
+    entry_points={
+        "console_scripts": [
+            "gitac = autocommit:main"
+        ]
+    },
+)
