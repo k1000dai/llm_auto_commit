38c3bdf58117399073267812f3332f9644936626
k1000dai <chiyodakku1000@gmail.com>
2024-05-02 14:06:41 +0900

    "Your simplified and understandable commit message based on the changes provided."

--- autocommit.py
+++ autocommit.py
@@ -2,70 +2,72 @@ from urllib.request import Request, urlopen
 import json
 import subprocess
 
-# git diff 取得
-cp = subprocess.run(['git', 'diff','--staged'], encoding='utf-8', stdout=subprocess.PIPE)
-diff = cp.stdout
+def autocommit():
+    # git diff 取得
+    cp = subprocess.run(['git', 'diff','--staged'], encoding='utf-8', stdout=subprocess.PIPE)
+    diff = cp.stdout
 
-url = "http://localhost:11434/api/generate"
+    url = "http://localhost:11434/api/generate"
 
-prompt = f"""
-<|user|>
-Write a simple and understadable commit message for the following changes:
+    prompt = f"""
+    <|user|>
+    Write a simple and understadable commit message for the following changes:
 
-# Changes
-change will be written here
+    # Changes
+    change will be written here
 
-Plese output with format like below
-<output>
-<commit_message>"your commit message"</commit_message>
-<misc>write some miscellaneous information</misc>
-</output>
-<|end|>
-<|assistant|>
-<output>
-<commit_message>Fix bug</commit_message>
-<misc>hoge</misc>
-</output>
-<|user|>
-Write a simple and understadable commit message for the following changes:
+    Plese output with format like below
+    <output>
+    <commit_message>"your commit message"</commit_message>
+    <misc>write some miscellaneous information</misc>
+    </output>
+    <|end|>
+    <|assistant|>
+    <output>
+    <commit_message>Fix bug</commit_message>
+    <misc>hoge</misc>
+    </output>
+    <|user|>
+    Write a simple and understadable commit message for the following changes:
 
-# Changes
-{diff}
+    # Changes
+    {diff}
 
-Plese output with format like below
-<output>
-<commit_message>"your commit message"</commit_message>
-<misc>write some miscellaneous information</misc>
-</output>
-<|assistant|>
-<output>
-<commit_message>
-"""
+    Plese output with format like below
+    <output>
+    <commit_message>"your commit message"</commit_message>
+    <misc>write some miscellaneous information</misc>
+    </output>
+    <|assistant|>
+    <output>
+    <commit_message>
+    """
 
-data = {
-  "model": "phi3",
-  "prompt": prompt,
-  "raw": True,
-  "stream": False,
-}
+    data = {
+    "model": "phi3",
+    "prompt": prompt,
+    "raw": True,
+    "stream": False,
+    }
 
-headers = {'Content-Type': 'application/json'}
+    headers = {'Content-Type': 'application/json'}
 
-request = Request(url, data=json.dumps(data).encode(), headers=headers)
-response = urlopen(request)
-            
-response_json = json.loads(response.read().decode())
-response_text = response_json['response']
+    request = Request(url, data=json.dumps(data).encode(), headers=headers)
+    response = urlopen(request)
+                
+    response_json = json.loads(response.read().decode())
+    response_text = response_json['response']
 
-# git commit にコミットメッセージを渡す
+    # git commit にコミットメッセージを渡す
+    try:
+        commit_message = response_text.split('</commit_message>')[0]
+        print(commit_message)
+        y_n = input('Do you want to commit? [y/n]: ')
+        if y_n == 'n':
+            exit()
+        subprocess.run(['git', 'commit', '-m', commit_message])
+    except Exception as e:
+        print(response_text)
 
-
-try:
-    commit_message = response_text.split('</commit_message>')[0]
-    print(commit_message)
-    y_n = input('Do you want to commit? [y/n]: ')
-    if y_n == 'n':
-        exit()
-    subprocess.run(['git', 'commit', '-m', commit_message])
-except Exception as e:
-    print(response_text)
+if __name__ == '__main__':
+    autocommit()
