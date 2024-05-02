4120f10098c279fa95936313fea2e6a5c0a6319b
k1000dai <chiyodakku1000@gmail.com>
2024-05-02 13:57:12 +0900

    tet

--- autocommit.py
+++ autocommit.py
@@ -4,23 +4,32 @@ import subprocess
 
 # git diff 取得
 cp = subprocess.run(['git', 'diff','--staged'], encoding='utf-8', stdout=subprocess.PIPE)
-diff = cp.stdout#.replace('\n', ' ')
+diff = cp.stdout
 
 url = "http://localhost:11434/api/generate"
 
-prompt = """
+prompt = f"""
 <|user|>
-Generate a concise git commit message written in present tense for the following code diff with the given specifications below
-Commit message must be a maximum of 10 characters.
-Exclude anything unnecessary such as translation. Your entire response will be passed directly into git commit.
+Write a simple and understadable commit message for the following changes:
 
-git diff --staged is as follows:
-{}
+# Changes
+{diff}
+
+Plese output with format like below
+<output>
+<commit_message>"your commit message"</commit_message>
+<misc>write some miscellaneous information</misc>
+</output>
+
+# Example
+<output>
+<commit_message>Fix bug</commit_message>
+<misc>hoge</misc>
+</output>
 
 <|end|>
 <|assistant|>
-""".format(diff)
-
+"""
 
 data = {
   "model": "phi3",
@@ -33,8 +42,11 @@ headers = {'Content-Type': 'application/json'}
 
 request = Request(url, data=json.dumps(data).encode(), headers=headers)
 response = urlopen(request)
-
+            
 response_json = json.loads(response.read().decode())
+response_text = response_json['response']
 
-print("-----------------")
-print(response_json['response'])
+# git commit にコミットメッセージを渡す
+commit_message = response_text.split('<commit_message>')[1].split('</commit_message>')[0]
+print(commit_message)
+subprocess.run(['git', 'commit', '-m', commit_message])
